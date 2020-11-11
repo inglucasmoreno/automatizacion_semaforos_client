622af7db6e360b0e88e4abc28165d5c9ab3c3a41
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2020-11-10 23:45:04 -0300

    Mantenimiento semaforos terminacion parcial

--- src/app/pages/semaforos/editar-semaforo.component.ts
+++ src/app/pages/semaforos/editar-semaforo.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SemaforosService } from '../../services/semaforos.service';
 import { Semaforo } from 'src/app/models/semaforo.model';
-import { delay } from 'rxjs/operators';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-editar-semaforo',
@@ -14,7 +14,8 @@ import { delay } from 'rxjs/operators';
 export class EditarSemaforoComponent implements OnInit {
 
   public semaforoForm: FormGroup;
-  private semaforo: Semaforo;
+  public semaforo: Semaforo = { _id: '', codigo: '' };
+  private id: string;
 
   constructor(private router: Router,
               private fb: FormBuilder,
@@ -22,18 +23,27 @@ export class EditarSemaforoComponent implements OnInit {
               private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe( ({id}) => this.getSamforo(id) );
-    console.log(this.semaforo);
+    this.activatedRoute.params.subscribe( ({id}) => {
+      this.id = id;
+      this.getSamforo(id);
+    });
     this.semaforoForm = this.fb.group({
       codigo: ['', Validators.required],
-      descripcion: ['', Validators.required]
+      descripcion: ['', Validators.required],
+      intermitente: [false, Validators.required],
+      activo: [false, Validators.required]
     });
   }
 
   getSamforo(id: string): void {
     this.semaforosService.getSemaforo(id).subscribe( semaforo => {
       this.semaforo = semaforo;
-      this.semaforoForm.setValue({codigo: this.semaforo.codigo, descripcion: this.semaforo.descripcion});
+      console.log(this.semaforo);
+      this.semaforoForm.setValue({
+          codigo: this.semaforo.codigo,
+          descripcion: this.semaforo.descripcion,
+          intermitente: this.semaforo.intermitente,
+          activo: this.semaforo.activo});
     });
   }
 
@@ -42,7 +52,22 @@ export class EditarSemaforoComponent implements OnInit {
   }
 
   actualizarSemaforo(): void{
-    console.log('actualizando semaforo...');
+    this.semaforosService.actualizarSemaforo(this.id, this.semaforoForm.value).subscribe(resp => {
+      Swal.fire({
+        title: 'Completado',
+        text: 'Actualización correcta!',
+        icon: 'success',
+        confirmButtonText: 'Entendido'
+      });
+      this.router.navigateByUrl('/dashboard/semaforos');
+    }, ({error}) => {
+      Swal.fire({
+        title: 'Información',
+        text: error.msg,
+        icon: 'info',
+        confirmButtonText: 'Entendido'
+      });
+    });
   }
 
 }
